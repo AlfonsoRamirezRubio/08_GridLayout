07a5660f2bc218e7e54c42a713f2389107e3a012
MartinDelyRey2002 <te200111636@tequila.tecmm.edu.mx>
2023-04-26 08:51:52 -0700

    calculadora que suma y resta usando un gridlayout

---
+++ app/src/main/java/com/example/a08_gridlayout/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.example.a08_gridlayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    lateinit var numeros: TextView
+    var cadena = ""
+    var primerNumero = 0
+    var operador = ""
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        numeros = findViewById(R.id.txtnumeros)
+
+    }
+    fun presiono(v: View){
+        val vista = v as TextView
+        Log.e("Boton", "presiono $(vista.text)")
+
+        when(vista.text) {
+            "C" -> {
+                cadena = ""
+                numeros.text = cadena
+                primerNumero = 0
+                operador = ""
+            }
+            "+", "-" -> {
+                if (cadena.isNotEmpty()) {
+                    primerNumero = cadena.toInt()
+                    operador = vista.text.toString()
+                    cadena = ""
+                }
+            }
+            "=" -> {
+                if (cadena.isNotEmpty() && operador.isNotEmpty()) {
+                    val segundoNumero = cadena.toInt()
+                    var resultado = 0
+                    when(operador) {
+                        "+" -> resultado = primerNumero + segundoNumero
+                        "-" -> resultado = primerNumero - segundoNumero
+                    }
+                    numeros.text = resultado.toString()
+                    cadena = ""
+                    operador = ""
+                }
+            }
+            else -> {
+                cadena += vista.text
+                numeros.text = cadena
+            }
+        }
+    }
+}
+
+
